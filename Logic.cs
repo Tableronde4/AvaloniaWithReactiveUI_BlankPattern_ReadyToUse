ff359a7826b604106cad1981ae11eec7b20efa36
Thomas Maitre <thomas2000.maitre@gmail.com>
2021-02-02 09:55:13 -0500

    Ajoutez des fichiers projet.

---
+++ Logic.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace TestAvaloniaBinding
+{
+    public class Logic
+    {
+        //instanciate Variable
+        public Variable variable = new Variable();
+        //
+        public void Main()
+        {
+            variable.State = 0;
+            variable.Pulse = 0;
+            
+            //this while is used to get a code that work indefinitely up to down
+            while (true)
+            {
+                variable.State++;
+
+                if (variable.Pulse == 0)
+                {
+                    variable.Pulse = 1;
+                }
+                else
+                {
+                    variable.Pulse = 0;
+                }
+
+
+                Thread.Sleep(100);
+            }
+        }
+
+        //each variable used in Logic.cs is declared here
+        public class Variable
+        {
+            public int State { get; set; }
+            public int Pulse { get; set; }
+        }
+    }
+}
